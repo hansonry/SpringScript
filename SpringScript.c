33fa027a553365bd63b239aa93fa31b6fc8211e7
Ryan Hanson <hansonry@gmail.com>
2016-03-20 12:35:05 -0500

    Map and mod

--- SpringScript.c
+++ SpringScript.c
@@ -2,6 +2,54 @@
 #include "ConfigParser.h"
 
 
+static int SpringScript_WriteGame(CPValue_T * value, FILE * output)
+{
+   int index;
+   CPValueObject_T * obj;
+   CPObjectPair_T * pair;
+   obj = &value->data.object;
+   // Write Map Name
+   index = ConfigParser_GetIndexOfKey(value, "map");
+   if(index >= 0)
+   {
+      pair = &obj->pair_list[index];
+      if(pair->value->type == e_CPVT_String)
+      {
+         fprintf(output, "   MapName=%s;\n", pair->value->data.string.token->str);
+      }
+      else
+      {
+         fprintf(stderr, "Error: Expected 'map' to be a value\n");
+      }
+   }
+   else
+   {
+      fprintf(stderr, "Error: Expected to find 'map'\n");
+   }
+
+   // Write Game Name
+   index = ConfigParser_GetIndexOfKey(value, "mod");
+   if(index >= 0)
+   {
+      pair = &obj->pair_list[index];
+      if(pair->value->type == e_CPVT_String)
+      {
+         fprintf(output, "   GameType=%s;\n", pair->value->data.string.token->str);
+      }
+      else
+      {
+         fprintf(stderr, "Error: Expected 'mod' to be a value\n");
+      }
+   }
+   else
+   {
+      fprintf(stderr, "Error: Expected to find 'mod'\n");
+   }
+
+   // Write IsHost
+   fprintf(output, "   IsHost=1;\n");
+
+}
 
 
 
@@ -11,13 +59,22 @@ int SpringScript_Run(Scanner_T * input, FILE * output)
    ConfigParser_T config;
    result = 1;
 
-   ConfigParser_Init(&config, input);
    fprintf(output, "[GAME]\n{\n");
-   
+   ConfigParser_Init(&config, input);
+
+   if(config.root->type == e_CPVT_Object)
+   {
+      SpringScript_WriteGame(config.root, output);
+   }
+   else
+   {
+      fprintf(stderr, "Error: Expected root value to be an object\n");
+   }
 
 
-   fprintf(output, "}\n");
+
    ConfigParser_Destory(&config);
+   fprintf(output, "}\n");
    return result;
 }
 
